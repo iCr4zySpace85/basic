da9e44e4560ee6eff2161a088d1a4ea5eb467d00
iCr4zySpace85 <iCr4zySpace85@gmail.com>
2023-02-28 11:41:39 -0600

    modified:   views/layouts/main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,9 +39,9 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'inicio', 'url' => ['/site/index']],
+            ['label' => 'acerca de', 'url' => ['/site/about']],
+            ['label' => 'Contactos', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
